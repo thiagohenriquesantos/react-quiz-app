878f9c41cad95b38819e126ef104744405eb12c1
Thiago Santos <thiagosantosdev@gmail.com>
2021-09-06 14:33:14 -0300

    home done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Home from './Pages/Home/Home';
+import Quiz from './Pages/Quiz/Quiz';
+import Result from './Pages/Result/Result';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app" style={{backgroundImage:'url(./ques1.png)'}}>
         <Header />
+        <Switch>
+          <Route path='/' exact>
+            <Home/>
+          </Route>
+          <Route path='/quiz' exact>
+            <Quiz/>
+          </Route>
+          <Route path='/result' exact>
+            <Result/>
+          </Route>
+        </Switch>
       </div>
       <Footer />
     </BrowserRouter>
