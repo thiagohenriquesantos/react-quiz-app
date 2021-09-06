96166a94f329e0853ed3a59eae4b9af962ee1929
Thiago Santos <thiagosantosdev@gmail.com>
2021-09-06 16:39:53 -0300

    quiz and questions done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import axios from "axios";
+import { useState } from "react";
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -7,16 +9,41 @@ import Quiz from './Pages/Quiz/Quiz';
 import Result from './Pages/Result/Result';
 
 function App() {
+  const [questions, setQuestions] = useState();
+  const [name, setName] = useState();
+  const [score, setScore] = useState(0);
+
+  const fetchQuestions = async (category = "", difficulty = "") => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10${
+        category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    );
+
+    setQuestions(data.results);
+  };
+
+
   return (
     <BrowserRouter>
       <div className="app" style={{backgroundImage:'url(./ques1.png)'}}>
         <Header />
         <Switch>
           <Route path='/' exact>
-            <Home/>
+          <Home
+            name={name}
+            setName={setName}
+            fetchQuestions={fetchQuestions}
+          />
           </Route>
           <Route path='/quiz' exact>
-            <Quiz/>
+            <Quiz
+              name={name}
+              questions={questions}
+              score={score}
+              setScore={setScore}
+              setQuestions={setQuestions}
+            />
           </Route>
           <Route path='/result' exact>
             <Result/>
